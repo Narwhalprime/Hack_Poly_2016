29540c827312cac0e9f76b1b29eb23ff6564d1ff
Panix <Panix>
2016-02-08 02:21:17 +0900

    Added health bar and changed model ratio

---
+++ Assets/HealthBar.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour 
+{
+    public Slider m_healthBar;
+
+	// Use this for initialization
+	void Start () 
+    {
+        m_healthBar.value = 75;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        m_healthBar.value = Instruction.health;
+	}
+}
